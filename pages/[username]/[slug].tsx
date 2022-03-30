3c311d716b91cc4dc7133edebfd368b9d18f5013
carlosayol <Carlosaolar@gmail.com>
2022-03-29 19:02:42 -0500

    add new page and componetn for posts

--- pages/[username]/[slug].tsx
+++ pages/[username]/[slug].tsx
@@ -1,3 +1,41 @@
+import { firestore, getUserWithUsername, postToJson } from "../../lib/firebase"
+
+export const getStaticProps = async ({ params }) => {
+  const { username, slug } = params
+  const userDoc = await getUserWithUsername(username)
+
+  let post
+  let path 
+
+  if (userDoc) {
+    const postRef = userDoc.ref.collection('posts').doc(slug)
+    post = postToJson(await postRef.get())
+
+    path = postRef.path
+  }
+
+  return {
+    props: { post, path },
+    revalidate: 5000
+  }
+}
+
+export const getStaticPaths = async () => {
+  const snapshot = await firestore.collectionGroup('posts').get()
+
+  const paths = snapshot.docs.map((doc) => {
+    const { slug, username } = doc.data()
+    return {
+      params: { username, slug }
+    }
+  })
+
+  return {
+    paths,
+    fallback: 'blocking'
+  }
+}
+
 export const PostPage = () => {
   return (
     <main>
